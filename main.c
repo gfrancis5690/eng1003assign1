129c2a2d29f521905a17e5a99c606c218b48e96b
gregory francis <c3111111@uon.edu.au>
2019-04-09 02:37:27 +0000

    alpha

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <stings.h>
-#include <sting.h>
-void encryp(char* , int N)
 
 
-int main(void)
-{
-    printf("hello, world\n"); // stuff
+
+int main(void){
+    int i;
+
+    for( i = 65; i < 90; i = i + 1 ){
+      printf("%c, %d\n", i, i);
+   }
+
+    return 0;
 }
