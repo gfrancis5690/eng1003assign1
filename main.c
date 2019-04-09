c43672da617509ba2b145b6748aa7898a02294ae
gregory francis <c3111111@uon.edu.au>
2019-04-09 03:55:04 +0000

    enc dec

--- main.c
+++ main.c
@@ -4,9 +4,22 @@
 
 int main(void){
     int i;
-
-    for( i = 65; i < 90; i = i + 1 ){
-      printf("%c, %d\n", i, i);
+    int temp;
+    int temp2;
+    int k = 1;
+    for( i = 65; i < 91; i = i + 1 )
+    {
+      temp = i-65;
+      temp = (temp + k)%26;
+      
+      temp2 = (temp - k)%26;
+      if(temp2 < 0)
+      {
+          temp2 = 25;
+          //I must have found Z, set temp2 to 'Z' value between 0-25
+      }
+      temp2 =  temp2 + 65;
+      printf("%c, enc: %d, %c, dec: %d %c\n", i, temp, temp+65, temp2-65, temp2);
    }
 
     return 0;
